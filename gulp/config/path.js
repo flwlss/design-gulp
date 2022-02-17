4d554fd4291d1b2e15a056869b808ba9ed9df99f
zxc1057 <dimaandreev1337@gmail.com>
2022-02-17 18:47:55 +0700

    scss

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -5,21 +5,24 @@ const buildFolder = `./dist`;
 const srcFolder = `./src`;
 
 export const path = {
-    build: {
-        html: `${buildFolder}/`,
-        files: `${buildFolder}/files/`
-    },
-    src: {
-        html: `${srcFolder}/*.html`,
-        files: `${srcFolder}/files/**/*.*`,
-    },
-    watch: {
-        html: `${srcFolder}/**/*.html`,
-        files: `${srcFolder}/files/**/*.*`
-    },
-    clean: buildFolder,
-    buildFolder: buildFolder,
-    srcFolder: srcFolder,
-    rootFolder: rootFolder,
-    ftp: ``,
+  build: {
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    files: `${buildFolder}/files/`
+  },
+  src: {
+    scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/*.html`,
+    files: `${srcFolder}/files/**/*.*`,
+  },
+  watch: {
+    scss: `${srcFolder}/scss/**/*.scss`,
+    html: `${srcFolder}/**/*.html`,
+    files: `${srcFolder}/files/**/*.*`
+  },
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  rootFolder: rootFolder,
+  ftp: ``,
 }
