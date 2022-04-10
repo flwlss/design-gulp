85378d89a2e0505c4bffa416528139f5f9b551ff
zxc1057 <dimaandreev1337@gmail.com>
2022-04-10 23:32:44 +0700

    about mobile

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -12,11 +12,31 @@ export function isWebp() {
   })
 }
 
+/// navBar change color
+/// navBar change color
+/// navBar change color
 window.onscroll = function () { scrollFunction() };
 export function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("headerNav").style.cssText = `background-color: white; transition: 0.5s`
   } else {
     document.getElementById("headerNav").style.cssText = `background-color: ; transition: 0.5s`
   }
+}
+
+// plavniy scroll
+// plavniy scroll
+// plavniy scroll
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById('menu-toggle').checked = false;
+    const blockID = anchor.getAttribute('href')
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
 }
