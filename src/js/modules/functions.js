979ee4c6fab1f11318495d56daf81a8437930cee
zxc1057 <dimaandreev1337@gmail.com>
2022-04-21 17:53:42 +0700

    services

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -40,3 +40,106 @@ for (let anchor of anchors) {
     })
   })
 }
+
+// open/close forms
+// open/close forms
+// open/close forms
+var arrowOne = document.querySelector(".arrow-one");
+var openOne = document.querySelector(".form-one");
+var closeOne = document.querySelector(".form-close-one");
+arrowOne.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openOne.classList.add("forma");
+});
+closeOne.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openOne.classList.remove("forma");
+});
+
+
+var arrowTwo = document.querySelector(".arrow-two");
+var openTwo = document.querySelector(".form-two");
+var closeTwo = document.querySelector(".form-close-two");
+arrowTwo.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openTwo.classList.add("forma");
+});
+closeTwo.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openTwo.classList.remove("forma");
+});
+
+
+var arrowThree = document.querySelector(".arrow-three");
+var openThree = document.querySelector(".form-three");
+var closeThree = document.querySelector(".form-close-three");
+arrowThree.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openThree.classList.add("forma");
+});
+closeThree.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openThree.classList.remove("forma");
+});
+
+
+var arrowFour = document.querySelector(".arrow-four");
+var openFour = document.querySelector(".form-four");
+var closeFour = document.querySelector(".form-close-four");
+arrowFour.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openFour.classList.add("forma");
+});
+closeFour.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openFour.classList.remove("forma");
+});
+
+
+var arrowFive = document.querySelector(".arrow-five");
+var openFive = document.querySelector(".form-five");
+var closeFive = document.querySelector(".form-close-five");
+arrowFive.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openFive.classList.add("forma");
+});
+closeFive.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openFive.classList.remove("forma");
+});
+
+
+var arrowSix = document.querySelector(".arrow-six");
+var openSix = document.querySelector(".form-six");
+var closeSix = document.querySelector(".form-close-six");
+arrowSix.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openSix.classList.add("forma");
+});
+closeSix.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openSix.classList.remove("forma");
+});
+
+
+
+var arrowSix = document.querySelector(".arrow-six");
+var openSix = document.querySelector(".form-six");
+var closeSix = document.querySelector(".form-close-six");
+arrowSix.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openSix.classList.add("forma");
+});
+closeSix.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openSix.classList.remove("forma");
+});
+
+// number mask
+// number mask
+// number mask
+var element = document.getElementById('phone');
+var maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+var mask = IMask(element, maskOptions);
